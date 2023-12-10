585dbd80dc7cf83131c8dfc3e2a587956f130837
Dulitha Wickramasinghe <110032696+Dulitha99@users.noreply.github.com>
2023-12-09 23:30:25 -0800

    changes4

--- portfolio/src/components/Navbar/Navbar.jsx
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import './Navbar.scss'
 
 export const Navbar = () => {
   return (
     <div>
         <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <b><div class="dulitha" href="#">Dulitha <span>Wickramasinghe</span></div></b>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
